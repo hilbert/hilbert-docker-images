017893d90d9d8e61ef3f8fd2ca0777b3367ad727
Oleksandr Motsak <motsak@mathematik.uni-kl.de>
2015-03-31 22:35:46 +0200

    Further corrections due to X11  & silence my_init messages for now (save for appA)

--- main.sh
+++ main.sh
@@ -15,17 +15,21 @@ case "$OSTYPE" in
      xauth nlist :0 | sed -e "s/^..../ffff/" | xauth -f $XAUTH nmerge -
   fi
   export X="DISPLAY -e USER -v $XSOCK:$XSOCK -v $XAUTH:$XAUTH -e XAUTHORITY=$XAUTH"
-  echo "Please make sure to run this script from your local X11 server (terminal) since xeys's X-client will use '$XSOCK, $XAUTH'..."  
+  echo "Please make sure to run this script from your local X11 server (terminal) since xeys's X-client will use '$XSOCK, $XAUTH'..."
+  echo
+  echo "We now enable anyone to connect to this X11..."
+  xhost + 
+  #localhost # this does not work :(
  ;;
  darwin*) # our X11 host ip on Mac OS X via boot2docker:
   ## $(boot2docker ip)
   export X="DISPLAY=192.168.59.3:0"
   echo "Please make sure to start xsocat.sh from your local X11 server since xeys's X-client will use '$X'..."
- ;;
+  echo "X11 should 'Allow connections from network clients & Your firewall should not block incomming connections to X11'"
+  # X11 instead of XQuartz?
+  open -a XQuartz # --args xterm $PWD/xsocat.sh #?
+ ;; 
 esac 
-
-# options for /sbin/my_init 
-OPTS="--skip-startup-files --skip-runit --no-kill-all-on-exit --quiet"
 
 #
 echo 
@@ -36,26 +40,32 @@ echo "Current docker images: "
 docker images -a
 
 echo "Pulling necessary images: "
-#docker pull "$U/$I:menu"
-#docker pull "$U/$I:appa"
-#docker pull "$U/$I:appb"
-#docker pull "$U/$I:xeyes"
+docker pull "$U/$I:menu"
+docker pull "$U/$I:appa"
+docker pull "$U/$I:appb"
+docker pull "$U/$I:xeyes"
 
 echo "Current docker containers: "
 docker ps -a
 
 echo
 
+# options for /sbin/my_init 
+OPTS="--skip-startup-files --no-kill-all-on-exit --quiet --skip-runit"
+
+# options for running terminal apps via docker run:
+export RUNTERM="--rm -it  -a stdin -a stdout -a stderr"
+
 while :
 do  
-  docker run --rm --name menu -a stdin -a stdout -a stderr -it "$U/$I:menu"
+  docker run $RUNTERM --name menu "$U/$I:menu" $OPTS -- /menu.sh
   APP="$?"
   # 201 -> a, 202 -> b, quite otherwise 
   # TODO: any better communication means?  
   case "$APP" in
-    201) docker run --rm --name  appA -i    "$U/$I:appa" ;;
-    202) docker run --rm --name  appB -i    "$U/$I:appb" ;;
-    203) docker run --rm --name xeyes -i -e $X "$U/$I:xeyes" $OPTS -- /usr/bin/xeyes ;;
+    201) docker run $RUNTERM --name  appA       "$U/$I:appa" ;;
+    202) docker run $RUNTERM --name  appB       "$U/$I:appb"  $OPTS -- /B.sh ;;
+    203) docker run --rm -it --name xeyes -e $X "$U/$I:xeyes" $OPTS -- /usr/bin/xeyes ;;
     *) echo "Thank You! Quiting... "; echo ; echo "Leftover containers: "; docker ps -a;  exit 0 ;;
   esac  
 done
