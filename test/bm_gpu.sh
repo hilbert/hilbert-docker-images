8d13b8524a955368d493ee8e716c26c25ffdf209
Oleksandr Motsak <motsak@mathematik.uni-kl.de>
2015-05-08 00:50:28 +0200

    Cleaning up the benchmarking scripts (GPU)

--- test/bm_gpu.sh
+++ test/bm_gpu.sh
@@ -3,15 +3,25 @@
 
 export LIBGL_DEBUG=verbose
 
-glxgears -fullscreen -info >> bm_glxgears.log 2>&1
+SELFDIR=`dirname "$0"`
+SELFDIR=`cd "$SELFDIR" && pwd`
 
-GpuTest-all.sh >> bm_gputest.log 2>&1
+# cd "$SELFDIR"
 
-glmark2 --annotate --fullscreen --off-screen >> bm_glmark2.log 2>&1
+test_x11.sh >> /tmp/.test.log 2>&1
+test_vbox.sh >> /tmp/.test.log 2>&1
 
-x11perf -sync -repeat 1 -all >> bm_x11perf.log 2>&1
-#### x11perf -sync -repeat 1 -dot -line100 -rect100 -circle100 -scroll100 -shmput100 -ftext >> bm_x11perf.log 2>&1
+glxgears -fullscreen -info >> /tmp/bm_glxgears.log 2>&1
 
+### The following 2 tests fail under VirtualBox with Guest Additions installed (and used for direct rendering)
+### due to insufficient LIBGL implementation of OpenGL :((( Using software rendering (MESA) will work but it is very slow!
+sh "$SELFDIR/GpuTest_Linux_x64_0.7.0/GpuTest-all.sh" >> /tmp/bm_gputest.log 2>&1
+glmark2 --annotate --fullscreen --off-screen >> /tmp/bm_glmark2.log 2>&1
+
+x11perf -sync -repeat 1 -dot -line100 -rect100 -circle100 -scroll100 -shmput100 -ftext >> /tmp/bm_x11perf.log 2>&1
+### x11perf -sync -repeat 1 -all >> /tmp/bm_x11perf.log 2>&1
+
+ls -al /tmp/*.log
 
 exit $?
 
