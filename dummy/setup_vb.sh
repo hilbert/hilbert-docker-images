68897f0595a13328b8b586f7108cd4e5548c61a5
Oleksandr Motsak <motsak@mathematik.uni-kl.de>
2015-06-04 22:57:33 +0200

    Improved :dummy

--- dummy/setup_vb.sh
+++ dummy/setup_vb.sh
@@ -14,25 +14,29 @@ VER=$(cat /sys/module/vboxvideo/version)
 # "$SELFDIR/test_vbox.sh"
 
 echo "Current Virtual Box 'vboxvideo'-module version: '$VER'"
-cd /tmp/ ;
 
-if [ ! -e "/tmp/VBoxLinuxAdditions.run" ]; then
-   echo "Downloading .iso..."
+cd /tmp/
 
-   test -e "/tmp/VBoxGuestAdditions_$VER.iso" || \
-   (  wget "http://download.virtualbox.org/virtualbox/$VER/VBoxGuestAdditions_$VER.iso"  || exit 1; )
+ISO="VBoxGuestAdditions_$VER.iso"
+RUN="VBoxGuestAdditions_$VER.run"
 
-   echo "Extracting .run..."
-   7z x "/tmp/VBoxGuestAdditions_$VER.iso" -y -bd -ir'!VBoxLinuxAdditions.run' || exit 1
+echo "Temps:"
+ls -la "/tmp/$RUN" "/tmp/$ISO"
 
-fi 
+if [ ! -e "/tmp/$RUN" ]; then
+   echo "Downloading $ISO..."
 
+   test -e "/tmp/$ISO" || \
+   (  wget "http://download.virtualbox.org/virtualbox/$VER/$ISO"  || exit 1; )
 
-mkdir -p /usr/lib/xorg/modules/drivers/ /usr/lib/x86_64-linux-gnu/dri/ && \
-    chmod go+rx /usr/lib/xorg/modules/drivers/ /usr/lib/x86_64-linux-gnu/dri/
+   echo "Extracting .RUN from `ls /tmp/$ISO`..."
+   rm -f /tmp/VBoxLinuxAdditions.run || true
+   7z x "/tmp/$ISO" -y -bd -ir'!VBoxLinuxAdditions.run' || exit 1
+   mv "/tmp/VBoxLinuxAdditions.run" "/tmp/$RUN"
+fi
 
-echo "Running .run..."
-sh /tmp/VBoxLinuxAdditions.run 2>&1 || true
+echo "Running $RUN..."
+sh "/tmp/$RUN" 2>&1 || true
 cat /var/log/vboxadd-install.log
 
 DEBIAN_FRONTEND=noninteractive apt-get install -fy
@@ -41,17 +45,23 @@ DEBIAN_FRONTEND=noninteractive apt-get install -fy
 # libx11-6 libxcomposite1 libxdamage1 libxext6 libxfixes3 libxmu6 libxt6 xserver-xorg-core
 # xorg-video-abi-18
 
-test -e /usr/lib/x86_64-linux-gnu/dri/vboxvideo_dri.so || \
-    ln -s /usr/lib/x86_64-linux-gnu/VBoxOGL.so \
-            /usr/lib/x86_64-linux-gnu/dri/vboxvideo_dri.so
+/etc/init.d/vboxadd-x11 restart
 
-test -e /usr/lib/xorg/modules/drivers/vboxvideo_drv.so || \
-    ln -s /usr/lib/x86_64-linux-gnu/VBoxGuestAdditions/vboxvideo_drv_115.so \
-            /usr/lib/xorg/modules/drivers/vboxvideo_drv.so
+#mkdir -p /usr/lib/xorg/modules/drivers/ /usr/lib/x86_64-linux-gnu/dri/ && \
+#    chmod go+rx /usr/lib/xorg/modules/drivers/ /usr/lib/x86_64-linux-gnu/dri/
 
-#echo "Cleaning up:" 
-#ls -la /tmp/VBoxLinuxAdditions.run "/tmp/VBoxGuestAdditions_$VER.iso"
-# rm -f /tmp/VBoxLinuxAdditions.run "/tmp/VBoxGuestAdditions_$VER.iso"
+#test -e /usr/lib/x86_64-linux-gnu/dri/vboxvideo_dri.so || \
+#    ln -s /usr/lib/x86_64-linux-gnu/VBoxOGL.so \
+#            /usr/lib/x86_64-linux-gnu/dri/vboxvideo_dri.so
+
+#test -e /usr/lib/xorg/modules/drivers/vboxvideo_drv.so || \
+#    ln -s /usr/lib/x86_64-linux-gnu/VBoxGuestAdditions/vboxvideo_drv_115.so \
+#            /usr/lib/xorg/modules/drivers/vboxvideo_drv.so
+
+echo "Temps:"
+ls -la "/tmp/$RUN" "/tmp/$ISO"
+
+#rm -f "/tmp/$RUN" "/tmp/$ISO"
 
 # VBOX_VERSION="$VER"
 echo "Setting up VBox LIBGL: '$VER' is done!" 
