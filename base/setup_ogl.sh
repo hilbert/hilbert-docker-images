073388cafb96a9ecb76a4f8b2ae361884f06db79
Oleksandr Motsak <motsak@mathematik.uni-kl.de>
2015-06-04 22:59:46 +0200

    Added setup_ogl.sh to :base

---
+++ base/setup_ogl.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+SELFDIR=`dirname "$0"`
+SELFDIR=`cd "$SELFDIR" && pwd`
+
+cd "$SELFDIR"
+
+SELFNAME=`basename "$0" .sh`
+
+GL="$@"
+
+T="$HOME/$SELFNAME.timestamp"
+
+if [ -e "$T" ]; then
+  echo "Please, run '$0' only once!"
+  exit 0
+fi  
+
+if [ -z "$GL" ]; then 
+  GL="/tmp/OGL.tgz"
+fi
+
+if [ -e "$GL" ]; then  
+  echo "Customizing using $GL..."
+  sudo tar xzvf "$GL" --skip-old-files -C / 
+  echo "Running ldconfig..."
+  sudo ldconfig 
+  
+  if [ -e "/etc/X11/Xsession.d/98vboxadd-xclient" ]; then 
+    echo "Trying to run '/etc/X11/Xsession.d/98vboxadd-xclient'..."
+    sudo sh /etc/X11/Xsession.d/98vboxadd-xclient || true
+  fi
+fi
+
+touch "$T"
+exit 0
+
