0848cb3df2461eb54e91defda95520bc40fe7996
Oleksandr Motsak <motsak@mathematik.uni-kl.de>
2015-06-08 00:12:59 +0200

    Build all & push all in setup.sh

--- setup.sh
+++ setup.sh
@@ -8,22 +8,19 @@ cd "$SELFDIR"
 ARGS="$@"
 
 if [ -z "$ARGS" ]; then
-    ARGS=" base dd main up appa appchoo alsa xeyes gui x11 play iceweasel skype q3 cups xbmc test "
+    ARGS=" base dd main dummy appa appchoo alsa xeyes gui x11 test cups play surfer iceweasel skype q3 xbmc up "
+# for dependency graph please check out depsgen.sh!
 fi
 
 U=malex984
 I=dockapp
 
-# docker pull phusion/baseimage:0.9.16
-#docker tag phusion/baseimage:0.9.16 "$U/$I:base"
-
+docker pull phusion/baseimage:0.9.16
 
 docker images -a
-docker ps -a -s
+docker ps -a
 
-# ALL: base dd main appa menu alsa xeyes gui play iceweasel skype q3 x11 cups ;
 
-# for dependencies please check out depsgen.sh!
 for d in $ARGS ;
 do
   echo
@@ -35,16 +32,17 @@ do
   docker ps -sa | grep "$d"
   # docker stop "$d"
   # docker rm -f "$d"
+  echo "Building '$d'..."
 
   make -C $PWD || docker build --pull=false --force-rm=true --rm=true -t "$U/$I:$d" "." || exit 1
+  docker rmi $(docker images -q -f dangling=true)
   
-  echo
+  echo "Pushing..."
 #  docker push "$U/$I:$d"
-#  make -C $PWD push 
+  make -C $PWD push 
   cd .. 
 done
 
-docker rmi $(docker images -q -f dangling=true)
 
 docker images -a
 docker ps -a
