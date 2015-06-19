f89d05338c2ddbfceaf9225628221f374f30bfe9
Oleksandr Motsak <motsak@mathematik.uni-kl.de>
2015-06-19 13:56:19 +0200

    Make use of CUPS_SERVER

--- cups/config_cups.sh
+++ cups/config_cups.sh
@@ -3,22 +3,37 @@
 I="$@"
 
 if [ -z "$I" ]; then
-   if [ ! -z "$PRINTER" ]; then 
-     I="$PRINTER"
+
+   if [ ! -z "$CUPS_SERVER" ]; then 
+      I="$CUPS_SERVER"
    else
-    if [ ! -z "$HIP" ]; then 
-       I="$HIP" 
-     else
-        I=$(ping -c 3 dockerhost | head -n 4 | tail -n 1 | sed -e 's@^.*(@@g'  -e 's@).*$@@g')
-        I="$I:631/version=1.1"
-     fi
+
+
+      if [ ! -z "$HIP" ]; then 
+         I="$HIP:631/version=1.1" 
+      else
+         ping -c 1 dockerhost || ( echo "Sorry - cannot determine the printer server!"; exit 1 )
+         I=$(ping -c 3 dockerhost | head -n 4 | tail -n 1 | sed -e 's@^.*(@@g'  -e 's@).*$@@g')
+         I="$I:631/version=1.1"
+      fi
+      export CUPS_SERVER="$I"
+
+   fi
+
+else
+
+   if [ ! -z "$CUPS_SERVER" ]; then 
+      echo "Overwriting '$I' with '$CUPS_SERVER'"
+      I="$CUPS_SERVER"
+   else
+      export CUPS_SERVER="$I"
    fi
 fi
 
-echo "Server: '$I'"
+echo "Server: '$I', CUPS_SERVER: '$CUPS_SERVER'"
 echo
 
-echo "ServerName $I" >> /etc/cups/client.conf
+echo "ServerName $I" > /etc/cups/client.conf
 
 lpstat -l -t -v -s -p 
 
