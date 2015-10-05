e4576589251dd1aaeed6eb275fd7830ce70c4baf
Oleksandr Motsak <motsak@mathematik.uni-kl.de>
2015-10-05 03:42:46 +0200

    Fixing X11vnc & menu settings

--- x11vnc/x11vnc.sh
+++ x11vnc/x11vnc.sh
@@ -1,18 +1,40 @@
 #!/bin/bash
 
-# PASS="$@"
-# P="${PASS:-1234}"
-# H="/tmp" ### FIXME! TODO: insecure
+if [ -z "${VNC_PASSWD}" ]; then 
+  VNC_PASSWD=`mkpasswd -l 5 -d 2`
+fi
+
+echo
+echo "VNC_PASSWD: ${VNC_PASSWD}"
+echo
+
+
+H="$HOME" 
+### FIXME! TODO: insecure
 
 # Setup a password
-# x11vnc -storepasswd "${P}" "$H/.vnc_passwd"
+mkdir -p "$H/.x11vnc/"
+
+F="$H/.x11vnc/passwd"
+
+echo "Passwd file: $F"
+x11vnc -storepasswd "${VNC_PASSWD}" "${F}"
 
 #x11vnc -usepw -forever -display "${DISPLAY}"
 
-# -localhost 
-x11vnc -display "${DISPLAY}" -auth "${XAUTHORITY}" -nopw -forever -rfbport 5900
-# -xkb -ncache 10 -listen -noxdamage -noxrecord -noxfixes -noscr -nowf -fixscreen
-# -o /tmp/x11vnc.log
+#### http://www.karlrunge.com/x11vnc/x11vnc_opts.html
+
+x11vnc -localhost -many -display "${DISPLAY}" \
+-xauth "${XAUTHORITY:-$HOME/.Xauthority}" -rfbauth "${F}" \
+-noxdamage -noxrecord -noxfixes -noscr -nowf \
+-xkb -nap -rfbport 5900 \
+-cursor most 
+# -wait 50 -safer \
+
+## -ncache 10
+## -listen
+# -fixscreen ? ? ?
+
 
 # remote end:
 # ssh -t -L 5900:localhost:5900 
