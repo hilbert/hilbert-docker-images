5b7663767176cdc79c947cc7d27cf1d4e800e510
Oleksandr Motsak <motsak@mathematik.uni-kl.de>
2015-11-12 01:57:45 +0100

    Updated :omd

--- omd/omd_entrypoint.sh
+++ omd/omd_entrypoint.sh
@@ -10,19 +10,22 @@ MAX_TRIES=5
 tries=0
 
 echo "** Starting OMD **"
-omd start
-service apache2 restart
+/etc/init.d/apache2 start 2>&1
+/etc/init.d/xinetd start 2>&1
+
+omd start 2>&1
+service apache2 restart 2>&1
 
 while /bin/true; do
   sleep $SLEEP_TIME
-  omd status | grep -q "stopped" && {
+  omd status 2>&1 | grep -q "stopped" && {
     if [ $tries -gt $MAX_TRIES ]; then
       echo "** ERROR: Stopped service found; aborting (after $tries tries) **"
       exit 1
     fi
     tries=$(( tries + 1 ))
     echo "** ERROR: Stopped service found; trying to start again **"
-    omd start
+    omd start 2>&1
   }
 done
 
