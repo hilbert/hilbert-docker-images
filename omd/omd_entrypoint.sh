beb9ad021f0c34425926378710223d5923c0e9de
Oleksandr Motsak <motsak@mathematik.uni-kl.de>
2016-01-12 02:43:54 +0100

    Experimenting with OMD config

--- omd/omd_entrypoint.sh
+++ omd/omd_entrypoint.sh
@@ -4,17 +4,33 @@
 
 echo "IP ADDR: `ip addr`"
 
-SLEEP_TIME=60
-MAX_TRIES=5
-
-tries=0
-
 echo "** Starting OMD **"
 /etc/init.d/apache2 start 2>&1
 /etc/init.d/xinetd start 2>&1
 
-omd start 2>&1
+omd start default 2>&1
 service apache2 restart 2>&1
+
+curl -L -XGET -u "omdadmin:omd" -- \
+"http://localhost:5000/default/check_mk/wato.py?mode=edit_configvar&varname=wato_upload_insecure_snapshots&site=&folder=" > /tmp/globs.html
+ID=`grep '_transid' /tmp/globs.html | sed -e 's|^.*_transid" value="\([0-9]*/[0-9]*\)".*$|\1|gi'`
+
+### Configuration GUI (WATO) / Allow upload of insecure WATO snapshots: OFF -> ON
+curl -L -XGET -u "omdadmin:omd" -- \
+"http://localhost:5000/default/check_mk/wato.py?folder=&mode=globalvars&_action=toggle&_varname=wato_upload_insecure_snapshots&_transid=$ID" > /tmp/wato_toggle.html
+
+#omdadmin/omd
+### http://stackoverflow.com/questions/12667797/using-curl-to-upload-post-data-with-files :(
+### TODO: FIXME: clean stop with backup????
+### TODO: FIXME: restore previous state?  TODO: updloate and restore...? or shared docker volume?
+# cmk -R ??
+
+# exit 0
+
+SLEEP_TIME=60
+MAX_TRIES=5
+
+tries=0
 
 while /bin/true; do
   sleep $SLEEP_TIME
@@ -29,5 +45,4 @@ while /bin/true; do
   }
 done
 
-
-## /bin/bash
+exit 0
