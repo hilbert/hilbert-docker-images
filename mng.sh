8d6db54cb4f291ad9d872da9cd94e2223cc9dbfa
Oleksandr Motsak <motsak@mathematik.uni-kl.de>
2016-05-04 14:11:58 +0200

    Adding mng.sh

---
+++ mng.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+SELFDIR=`dirname "$0"`
+SELFDIR=`cd "$SELFDIR" && pwd`
+cd $SELFDIR
+
+lsof -n -i | grep -E ':(9000|8080)'
+
+cd STATIONS && ../www.sh >../www.log 2>&1 & 
+cd $SELFDIR && socat TCP4-LISTEN:9000,fork EXEC:./bashttpd >bashttpd.log 2>&1 &
+
+tail -f bashttpd.log
+
+
