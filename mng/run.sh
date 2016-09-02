7a1bde39cfb7dc44891b34d27f4402a1ddbdd372
Oleksandr Motsak <motsak@mathematik.uni-kl.de>
2016-09-02 16:53:45 +0200

    Adding simulation demo as a standard execution target

--- mng/run.sh
+++ mng/run.sh
@@ -17,7 +17,7 @@ nginx
 
 cd /usr/local/dockapp_dashboard/server
 export NODE_PATH=/usr/lib/node_modules/dockapp-dashboard-server/node_modules
-exec node app/main.js --log_level=verbose
+exec node app/main.js --log_level=verbose "$*"
 
 #  -s signal     : send signal to a master process: stop, quit, reopen, reload
 
