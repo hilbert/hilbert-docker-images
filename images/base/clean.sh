7cc7531248e96e2c48121b40ab67c7ec439a84e2
Oleksandr Motsak <http://goo.gl/mcpzY>
2017-02-05 12:39:10 +0100

    Run wrappers for apt-get actions via /bin/sh (instead of /bin/bash)
    *install.sh to avoid installing recommended packages as a rule
    * minor cleanup

--- images/base/clean.sh
+++ images/base/clean.sh
@@ -1,7 +1,5 @@
-#! /bin/bash
+#! /bin/sh
 
 # ubuntu/debian
 DEBIAN_FRONTEND=noninteractive apt-get -y -q clean "$@" && rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
-
-## DEBIAN_FRONTEND=noninteractive apt-get -y -q update -qqy --force-yes "$@"
 
