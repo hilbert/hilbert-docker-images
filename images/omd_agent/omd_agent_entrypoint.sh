29c1b041abc2ddcf1ab3709746a3467c84d4e81c
Oleksandr Motsak <malex984@users.noreply.github.com>
2018-03-13 01:51:41 +0100

    Use released HB stuff (v1.0.0) in docker images (/base, /kiosk, /omd_agent) directly
    * also some minor cleanup
    
    NOTE: `/omd_agent` is now directly based on `/base` (instead of `/dd`)

--- images/omd_agent/omd_agent_entrypoint.sh
+++ images/omd_agent/omd_agent_entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 /etc/init.d/xinetd restart
-exec python3 /usr/local/bin/heartbeat3.py 'server'
+exec python3 /usr/local/bin/heartbeat.py 
+# 'server'
 #python2.7 /tmp/heartbeat2.py  'server'
 
