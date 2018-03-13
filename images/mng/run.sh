a67c514ecbe0e698fdf777b1d9ab6c8fd906a0e3
Oleksandr Motsak <malex984@users.noreply.github.com>
2018-03-13 01:55:10 +0100

    Updating `hilbert/mng` due to new changes in UI
    * removed wrapper scripts (since Dashboard uses `hilbert` CLI tool directly)
    * major change: use command-line switches in `run.sh`
    * removed `uiserver.sh` in favour of more powerful `run.sh`
    
    NOTE: `hilbert` server side tool (ruamel.yaml) requires correct UTF-8 local setting

--- images/mng/run.sh
+++ images/mng/run.sh
@@ -21,6 +21,77 @@ fi
 ## set -v
 ## set -x 
 
+# configure nginx
+: ${HILBERT_UI_EXT_PORT:=8000}
+: ${HILBERT_UI_INT_PORT:=4000}
+cat <<EOF > /etc/nginx/sites-enabled/hilbert-ui
+server {
+       listen       ${HILBERT_UI_EXT_PORT};
+       server_name  localhost;
+
+       location / {
+           root   /usr/local/hilbert-ui/client/public;
+           index index.html index.htm;
+       }
+
+       location /api/ {
+               proxy_pass http://127.0.0.1:${HILBERT_UI_INT_PORT}/;
+               proxy_redirect default;
+               proxy_http_version 1.1;
+               proxy_set_header Upgrade \$http_upgrade;
+               proxy_set_header Connection 'upgrade';
+               proxy_set_header Host \$host;
+               proxy_cache_bypass \$http_upgrade;
+       }
+
+       error_page   500 502 503 504  /50x.html;
+       location = /50x.html {
+           root   html;
+       }
+}
+EOF
+
 /usr/local/nginx.sh || exit 1
 
-exec /usr/local/uiserver.sh $@
+: ${HILBERT_UI_SIMULATION_MODE:=0}
+: ${HILBERT_UI_LOG_LEVEL:=info}
+: ${HILBERT_UI_LOG_DIR:=./log}
+: ${HILBERT_UI_OMD_POLL_DELAY:=1000}
+: ${HILBERT_UI_POOL_TIMEOUT:=15}
+: ${HILBERT_UI_OMD_CMD:=nc localhost 6557}
+: ${HILBERT_UI_DB:=../work/db/hilbert-dev.sqlite}
+: ${HILBERT_SERVER_CONFIG_PATH:=/HILBERT/}
+: ${HILBERT_CLI_PATH:=/usr/local/bin}
+
+cd /usr/local/hilbert-ui/server/
+
+cat <<EOF
+Starting Dashboard's Back-end Server [node app/main.js] (in [${PWD}]) with the following arguments:
+  --test="${HILBERT_UI_SIMULATION_MODE}"
+  --port="${HILBERT_UI_INT_PORT}"
+  --log_level="${HILBERT_UI_LOG_LEVEL}"
+  --log_directory="${HILBERT_UI_LOG_DIR}"
+  --mkls_poll_delay="${HILBERT_UI_OMD_POLL_DELAY}"
+  --long_poll_timeout="${HILBERT_UI_POOL_TIMEOUT}"
+  --mkls_cmd="${HILBERT_UI_OMD_CMD}"
+  --db_path="${HILBERT_UI_DB}"
+  --hilbert_cfg="${HILBERT_SERVER_CONFIG_PATH}"
+  --hilbert_cli="env LC_CTYPE=en_US.UTF-8 ${HILBERT_CLI_PATH}/hilbert"
+  "$@"
+
+$(env | grep HILBERT_)
+EOF
+
+# export NODE_PATH=/usr/lib/node_modules/hilbert-ui/node_modules
+exec node app/main.js \
+  --test="${HILBERT_UI_SIMULATION_MODE}" \
+  --port="${HILBERT_UI_INT_PORT}" \
+  --log_level="${HILBERT_UI_LOG_LEVEL}" \
+  --log_directory="${HILBERT_UI_LOG_DIR}" \
+  --mkls_poll_delay="${HILBERT_UI_OMD_POLL_DELAY}" \
+  --long_poll_timeout="${HILBERT_UI_POOL_TIMEOUT}" \
+  --mkls_cmd="${HILBERT_UI_OMD_CMD}" \
+  --db_path="${HILBERT_UI_DB}" \
+  --hilbert_cfg="${HILBERT_SERVER_CONFIG_PATH}" \
+  --hilbert_cli="env LC_CTYPE=en_US.UTF-8 ${HILBERT_CLI_PATH}/hilbert" \
+  "$@"
--- images/mng/uiserver.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-cd /usr/local/hilbert-ui/server/
-
-echo "Starting Dashboard's Back-end Server (in [${PWD}]) with the following arguments: '$*'"
-# export NODE_PATH=/usr/lib/node_modules/hilbert-ui/node_modules
-# exec node main.js $@
-exec node app/main.js $@
