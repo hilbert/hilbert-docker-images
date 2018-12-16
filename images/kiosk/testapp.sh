1fa3a58b4ddb7df598d6defdd615ad8c9e402071
Oleksandr Motsak <malex984@users.noreply.github.com>
2018-12-16 23:49:39 +0100

    Update Kiosk-Browser to version `0.10.0-alpha.1`

--- images/kiosk/testapp.sh
+++ images/kiosk/testapp.sh
@@ -3,4 +3,4 @@
 SELFDIR=`dirname "$0"`
 SELFDIR=`cd "$SELFDIR" && pwd`
 
-exec "$SELFDIR/browser.sh" --verbose --verbose --verbose --verbose --integration --testapp --localhost --fullscreen --kiosk --always-on-top "$@"
+exec "$SELFDIR/browser.sh" --verbose --verbose --verbose --verbose --integration --localhost --fullscreen --kiosk --always-on-top -l kiosk:\\testapp "$@"
